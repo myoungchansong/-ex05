b6a28b9de45c57b1448b7274a3797ccf5526099c
myoungchansong <audcks68@gmail.com>
2019-01-30 12:28:34 +0900

    Ajax를 이용한 파일 업로드

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -24,6 +24,8 @@ public class UploadController {
 	@PostMapping("/uploadFormAction")
 	public void  uploadFormPost(MultipartFile[] uploadFile, Model model) {
 		
+		String uploadFolder = "C:\\upload";
+		
 		for(MultipartFile multipartFile : uploadFile) {
 			
 			log.info("-------------------------------");
@@ -32,7 +34,95 @@ public class UploadController {
 
 			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
 			
+			try {
+				
+				multipartFile.transferTo(saveFile);
+//				MultipartFile 의 transferTo() 라는 메소드를 사용해서 원하는 위치에 저장
+
+				
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		
+		log.info("upload Ajax");
+		
+		
+	}
+	
+	
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("UPDATE Ajax post......");
+		
+		String uploadFolder = "C:\\upload";
+		
+		for (MultipartFile multipartFile : uploadFile) {
+		
+			log.info("--------------------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			//IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") +1);
+			log.info("only file name : " + uploadFileName);
+			
+			File saveFile = new File(uploadFolder, uploadFileName);
+			
+			try {
+				
+				multipartFile.transferTo(saveFile);
+				
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			
+			}
+			
+			
+			
 		}
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
